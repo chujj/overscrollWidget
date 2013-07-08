305bc781448215f55f1a04846f86c74f21e8c3db
JiaJun Chu <ltaiyuezjj@gmail.com>
2013-07-08 20:48:05 +0800

    横竖屏， 不newView

--- src/com/example/waterfall/MainActivity.java
+++ src/com/example/waterfall/MainActivity.java
@@ -6,14 +6,27 @@ import com.ds.widget.ScrollOverPanel.IModel;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.ViewGroup;
 
 public class MainActivity extends Activity {
 
+	static ScrollOverPanel mPanel;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		IModel model = new ColumnListView(this);
-		this.setContentView(new ScrollOverPanel(this, model));
+		if (mPanel == null) {
+			IModel model = new ColumnListView(this);
+			mPanel = (new ScrollOverPanel(this, model));
+		}
+
+		this.setContentView(mPanel);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		
+		((ViewGroup) mPanel.getParent()).removeView(mPanel);
 	}
 
 }
