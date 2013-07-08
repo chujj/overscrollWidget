b911e6220ce16f4a6a29edce17cddbd5d2af3ca0
JiaJun Chu <ltaiyuezjj@gmail.com>
2013-07-08 18:08:42 +0800

    填充几个函数

--- src/com/example/waterfall/ColumnListView.java
+++ src/com/example/waterfall/ColumnListView.java
@@ -15,6 +15,155 @@ public class ColumnListView implements IModel {
 	
 	private ItemDrawable[] mItems;
 
+	public ColumnListView(Context context) {
+		int size = BITMAPS.length;
+		mItems = new ItemDrawable[size];
+		for (int i = 0; i < size; i++) {
+			mItems[i] = new ItemDrawable(context, BITMAPS[i]);
+		}
+	}
+	
+	
+	private void layout(int aTotalWidth) {
+		int l, t, r, b;
+		int[] columns_height = new int[UI_COLUMNT];
+		int singleWidth = (int) (aTotalWidth * 1.0 / UI_COLUMNT);
+		
+	}
+	
+	@Override
+	public IModelItem[] getItemLists() {
+		return mItems;
+	}
+
+	@Override
+	public int getSingleHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void reloadData() {
+		throw new RuntimeException("cannot reloadData");
+	}
+
+	@Override
+	public int getTotalHeight() { 
+		int last = mItems.length - 1;
+		return Math.max(mItems[last].mBottom, mItems[last - 1].mBottom);// mustly it hit 
+	}
+
+	@Override
+	public IModelItem[] getVisiableItems(int aFromY, int aToY) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int hitWhichItem(int aY) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void onOverTop() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onOverBottom() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRegionRelease(int aFromY, int aToY) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static class ItemDrawable extends Drawable implements IModelItem {
+
+		Bitmap mBitmap;
+		int mHeight, mWidth;
+		int mLeft, mTop, mRight, mBottom;
+		int mLeftMargin, mTopMargin, mRightMargin, mBottomMargin;
+		
+		public ItemDrawable(Context context, int aResId) {
+			mBitmap = BitmapFactory.decodeResource(context.getResources(), aResId);
+			mHeight = mBitmap.getHeight();
+			mWidth = mBitmap.getWidth();
+		}
+
+		public void setMargin(int aBorderMargin) {
+			mLeftMargin = mTopMargin = mRightMargin = mBottomMargin = aBorderMargin;
+		}
+		
+		@Override
+		public void setBounds(int left, int top, int right, int bottom) {
+			mLeft = left;
+			mTop = top; 
+			mRight = right;
+			mBottom = bottom;
+		}
+
+		@Override
+		public void drawSelf(Canvas aCanvas, int aStart, int aEnd, int aTotalWidth) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keydown(int aOffsetX, int aOffsetY) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyUpCancel() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onClick() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean onLongClick() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void draw(Canvas canvas) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public int getOpacity() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public void setAlpha(int alpha) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setColorFilter(ColorFilter cf) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
+	
 	private static final int[] BITMAPS = {
 		R.drawable.home_icon_163,
 		R.drawable.home_icon_application_center,
@@ -82,127 +231,4 @@ public class ColumnListView implements IModel {
 		R.drawable.land_home_icon_video,
 		R.drawable.land_home_icon_weibo,
 	};
-	
-	public ColumnListView(Context context) {
-		int size = BITMAPS.length;
-		mItems = new ItemDrawable[size];
-		for (int i = 0; i < size; i++) {
-			mItems[i] = new ItemDrawable(context, BITMAPS[i]);
-		}
-	}
-	
-	@Override
-	public IModelItem[] getItemLists() {
-		return mItems;
-	}
-
-	@Override
-	public int getSingleHeight() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void reloadData() {
-		throw new RuntimeException("cannot reloadData");
-	}
-
-	@Override
-	public int getTotalHeight() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public IModelItem[] getVisiableItems(int aFromY, int aToY) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int hitWhichItem(int aY) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void onOverTop() {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void onOverBottom() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onRegionRelease(int aFromY, int aToY) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public static class ItemDrawable extends Drawable implements IModelItem {
-
-		Bitmap mBitmap;
-		public ItemDrawable(Context context, int aResId) {
-			mBitmap = BitmapFactory.decodeResource(context.getResources(), aResId);
-		}
-		
-		@Override
-		public void drawSelf(Canvas aCanvas, int aStart, int aEnd, int aTotalWidth) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void keydown(int aOffsetX, int aOffsetY) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void keyUpCancel() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void onClick() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public boolean onLongClick() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public void draw(Canvas canvas) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public int getOpacity() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public void setAlpha(int alpha) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void setColorFilter(ColorFilter cf) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-	}
-
 }
